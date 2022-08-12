26a6c9862c41dec2dd28ef15104a2465a69ee0d5
Kartik Tiwari <kartik.tiwari@alumni.ashoka.edu.in>
2022-08-12 13:24:53 +0530

    added correction term

--- modules/approximations.py
+++ modules/approximations.py
@@ -1,4 +1,6 @@
-from numpy import arccos, cos
+# curve-fit() function imported from scipy
+
+from numpy import arccos, cos, exp
 
 '''
 Note: Input R in r_G normalized terms
@@ -6,3 +8,8 @@ Note: Input R in r_G normalized terms
 
 def beloApprox(alpha, R):
     return arccos(1-((1-cos(alpha))/(1-(1/R))))
+
+def beloApproxCorrected(alpha, R):
+    A, B, C = 0.0026091626179161505, 8.896382706025112e-06, 5.350133183173007
+    return arccos(1-((1-cos(alpha))/(1-(1/R)))) - (A + B*exp(C*alpha))
+
