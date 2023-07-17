64a402f00dbdd9b00db4ef8ae994dc4c685f5c33
Kartik Tiwari <kartik.tiwari@alumni.ashoka.edu.in>
2023-07-17 20:08:49 +0530

    Latest simulation result's data dump

--- modules/polarizationtransport.py
+++ modules/polarizationtransport.py
@@ -27,7 +27,7 @@ Output: Total flux of the star in that phase
 '''
 def F_I(phase, star, E, res='low'):
     
-    I_o, I_e = 1, 0
+    I_o, I_e = 0, 1
     
     # unpacking star parameters
     R, star_eta, star_i = star.radius, star.eta, star.i
@@ -37,12 +37,15 @@ def F_I(phase, star, E, res='low'):
     else:
         dtheta, dphi = 0.05, 0.05      # Higher Resolution for final
 
-    phiLim = alpha2psiCorrected(np.pi/2, R)
+    # extracting correction coefficients
+    corr_coeff = extract_coefficients(R) 
+    phiLim = np.deg2rad(160)   # even in the most compact neutron stars (R = 2), lensing doesn't lead to seeing patches beyond 160deg
+    
     # Initializing Arrays
     thetaRange = np.arange(0, np.pi, dtheta)
     phiRange = np.arange(-phiLim, phiLim, dphi)
 
-    dA = R**2*dtheta*dphi       # unit area element
+    dA = R**2*dtheta*dphi      # unit area element
     u = 1/R                    # ratio R_g/R (R already in terms of schwarzschild radius)   
     g_R = np.sqrt(1 - (u))     # gravitational redshift parameter
     
@@ -52,23 +55,25 @@ def F_I(phase, star, E, res='low'):
     for i in range(0, len(thetaRange)):
         for j in range(0, len(phiRange)):
             
-            ## calculate psi (using belo function)
-            a = psi2alphaCorrected((np.pi/2 - phiRange[j]), R)         
+            a = fastApprox((phiRange[j]), R, corr_coeff)[0]         
+            
+            if a>=np.pi/2:
+                pass    
                         
-            ## compute flux in Q stokes parameter from this area element
+            ## compute flux from this area element
             F.append(g_R*dA*np.cos(a)*(I_o+I_e))
             
-    return np.sum(F)
+    return np.nansum(F)
 
 '''
 Function to integrate stokes parameter over the visible part of the star (with even constant intensity distribution)
 Input: phase, star object, energy, resolution (default = 'low')
-Ouput: FQ, FU (stokes parameters)
+Ouput: FQ, FU, FI (stokes parameters and total intensity)
 '''
 def stokes(phase, star, E, res='low'): 
     
     I_o, I_e = 1, 0
-    
+        
     if res == 'low':
         dtheta, dphi = 0.1, 0.1        # Low Resolution for testing
     else:
@@ -86,7 +91,10 @@ def stokes(phase, star, E, res='low'):
     phiRange = np.arange(-phiLim, phiLim, dphi)
 
     dA = R**2*dtheta*dphi       # unit area element
-    Theta = np.arccos(np.cos(star_eta)*np.cos(star_i) + np.sin(star_eta)*np.sin(star_i)*np.cos(phase))    # angle between LOS and magnetic axis [WARNING: Theta is not the same as theta AND I am not sure whether this is the correct relation]
+    eta = np.arccos(np.round((np.cos(star_eta)*np.cos(star_i) + np.sin(star_eta)*np.sin(star_i)*np.cos(phase)), 12))    
+ 
+    ## Defining m_hat vector components in x, y, z coordinates (eq 16 - pavlov2000)
+    m_hat = np.array([np.sin(eta), 0, np.cos(eta)])
     
     u = 1/R                    # ratio R_g/R (R already in terms of schwarzschild radius)   
     g_R = np.sqrt(1 - (u))     # gravitational redshift parameter
@@ -94,54 +102,147 @@ def stokes(phase, star, E, res='low'):
     # GR correction to magnetic dipole moment (eq. 14 - pavlov2000)
     f = 2*(u**2 - 2*u - 2*(1-u)*np.log(1-u))/(np.sqrt(1-u)*(u**2 + 2*u + 2*np.log(1-u)))
     
-    FQ, FU = [], []                     # empty list to store flux for each patch
+    FQ, FU, F = [], [], []                     # empty list to store flux for each patch
     
     # from each alpha and theta
     for i in range(0, len(thetaRange)):
         for j in range(0, len(phiRange)):
             
             ## calculate psi (using belo function)
-            a = fastApprox((np.pi/2 - phiRange[j]), R, corr_coeff)[0]         
+            a = fastApprox((phiRange[j]), R, corr_coeff)[0]      
+            beta = phiRange[j] - a
             
-            if a>=np.pi/2:
-                pass
-            
-            ### r_hat vector components in x, y, z coordinates (eq 15 - pavlov2000)
-            r_hat = np.array([np.sin(phiRange[j])*np.cos(thetaRange[i]), np.sin(phiRange[j])*np.sin(thetaRange[i]), np.cos(phiRange[j])])
-                        
-            ### Defining component transformation matrix 
-            T = np.array([[np.cos(thetaRange[i])*np.cos(phiRange[j] - a), np.sin(thetaRange[i])*np.cos(phiRange[j] - a), -np.sin(phiRange[j] - a)],
-                           [-np.sin(thetaRange[i]), np.cos(thetaRange[i]), 0],
-                           [np.cos(thetaRange[i])*np.sin(phiRange[j] - a), np.sin(thetaRange[i])*np.sin(phiRange[j] - a), np.cos(phiRange[j] - a)]])
-            
-            ### r_hat vector components  in x', y', z' coordinates (eq 8-10 - pavlov2000)
-            # r_hat_prime = np.dot(T, r_hat) 
-            
-            ## Defining m_hat vector components in x, y, z coordinates (eq 16 - pavlov2000)
-            m_hat = np.array([np.sin(Theta), 0, np.cos(Theta)])
-            
-            ## get magnetic field at all points (eq 13 - pavlov2000) 
-            B_vec = (B_p/2)*((2+f)*np.dot(r_hat, m_hat)*r_hat - f*m_hat)  # in the basis vectors of (x,y,z)
-            B_vec_prime = np.dot(T, B_vec)                                # in the prime coordinates (x', y', z')
-            
-            ## find phi' using By' and Bx'.  
-            phi_prime = np.arctan2(B_vec_prime[1], B_vec_prime[0])               # phi' is the angle between the x' axis and the projection of B_vec_prime onto the x'y' plane
+            cosa = np.cos(a)
+             
+            if cosa>0:
+                                       
+                # computing trig functions once for each patch
+                costheta = np.cos(thetaRange[i])
+                sintheta = np.sin(thetaRange[i])
+                cosbeta = np.cos(beta)
+                sinbeta = np.sin(beta)
+                sinphi = np.sin(phiRange[j])
+                cosphi = np.cos(phiRange[j])      
+                
+                ### r_hat vector components in x, y, z coordinates (eq 15 - pavlov2000)
+                r_hat = np.array([sinphi*costheta, sinphi*sintheta, cosphi])
+                
+                ### Defining component transformation matrix 
+                T = np.array([[costheta *cosbeta, sintheta*cosbeta, -sinbeta],
+                            [-sintheta, costheta, 0],
+                            [costheta*sinbeta, sintheta*sinbeta, cosbeta]])
+                
+                ### r_hat vector components  in x', y', z' coordinates (eq 8-10 - pavlov2000)
+                # r_hat_prime = np.dot(T, r_hat) 
 
-            ## angle between magnetic field and the unit wave vector at the surface
-            Theta_prime = np.arccos(B_vec_prime[2]/np.linalg.norm(B_vec_prime))  
-            
-            ## angle between B_vec and normal to the surface
-            Theta_B = np.arccos(np.dot(B_vec/np.linalg.norm(B_vec), r_hat))
-            B = B_p * f / np.sqrt(4 - (4 - f**2)*np.cos(Theta_B)**2)  # magnetic field strength at the surface (eq. 17 - pavlov2000)
-            
-            # degree of linear polarization
-            p_L = linear_polarization(Theta_prime, B, g_R*E)
-            
-            ## compute flux in Q stokes parameter from this area element
-            FQ.append(g_R*dA*np.cos(a)*(I_o-I_e)*p_L*np.cos(2*(thetaRange[i] - phi_prime)))
-            FU.append(g_R*dA*np.cos(a)*(I_e-I_o)*p_L*np.sin(2*(thetaRange[i] - phi_prime)))
+                ## get magnetic field at all points (eq 13 - pavlov2000) 
+                B_vec = (B_p/2)*((2+f)*np.dot(r_hat, m_hat)*r_hat - f*m_hat)  # in the basis vectors of (x,y,z)
+                B_vec_prime = np.dot(T, B_vec)                                # in the prime coordinates (x', y', z')
+       
+                ## find phi' using By' and Bx'.  
+                xi_prime = np.arctan2(B_vec_prime[1], B_vec_prime[0])               # phi' is the angle between the x' axis and the projection of B_vec_prime onto the x'y' plane
+                
+                ## angle between B_vec and normal to the surface
+                # gamma = np.dot(r_hat, m_hat)
+                # B = (B_p/2) * np.sqrt((4 - f**2)*np.cos(gamma)**2 + f**2)  # magnetic field strength at the surface (eq. 17 - pavlov2000)
+                B = np.linalg.norm(B_vec)
+                
+                ## angle between magnetic field and the unit wave vector at the surface
+                Theta_prime = np.arccos(B_vec_prime[2]/B)  
+                              
+                # degree of linear polarization
+                p_L = linear_polarization(Theta_prime, B, g_R*E)
+                
+                ## compute flux in Q stokes parameter from this area element
+                FQ.append(g_R*dA*cosa*(I_o-I_e)*p_L*np.cos(2*(thetaRange[i] + xi_prime)))
+                FU.append(g_R*dA*cosa*(I_o-I_e)*p_L*np.sin(2*(thetaRange[i] + xi_prime)))
+                F.append(g_R*dA*cosa*(I_o+I_e))
 
-    return np.nansum(FQ), np.nansum(FU)
+
+    return np.nansum(FQ), np.nansum(FU), np.nansum(F)
+
+
+###############################################################
+#### STOKES TRANSFER WITHOUT GRAVITATIONAL LENSING ############
+###############################################################
+'''
+Function to integrate stokes parameter over the visible part of the star (with even constant intensity distribution)
+Input: phase, star object, energy, resolution (default = 'low')
+Ouput: FQ, FU, FI (stokes parameters and total intensity)
+'''
+def stokes_nolensing(phase, star, E, res='low'): 
+    
+    I_o, I_e = 1, 0
+        
+    if res == 'low':
+        dtheta, dphi = 0.1, 0.1        # Low Resolution for testing
+    else:
+        dtheta, dphi = 0.05, 0.05      # Higher Resolution for final
+        
+    # unpacking star parameters
+    R, star_eta, star_i, B_p = star.radius, star.eta, star.i, star.B_p     
+    
+    # Initializing Arrays
+    thetaRange = np.arange(0, np.pi, dtheta)
+    phiRange = np.arange(-np.pi/2, np.pi/2, dphi)
+
+    dA = R**2*dtheta*dphi       # unit area element
+    eta = np.arccos(np.round((np.cos(star_eta)*np.cos(star_i) + np.sin(star_eta)*np.sin(star_i)*np.cos(phase)), 12))    # angle between LOS and magnetic axis [WARNING: Theta is not the same as theta AND I am not sure whether this is the correct relation]
+ 
+    ## Defining m_hat vector components in x, y, z coordinates (eq 16 - pavlov2000)
+    m_hat = np.array([np.sin(eta), 0, np.cos(eta)])
+    
+    u = 1/R                    # ratio R_g/R (R already in terms of schwarzschild radius)   
+    g_R = np.sqrt(1 - (u))     # gravitational redshift parameter
+        
+    # GR correction to magnetic dipole moment (eq. 14 - pavlov2000)
+    f = 1
+    
+    FQ, FU, F = [], [], []                     # empty list to store flux for each patch
+    
+    # from each alpha and theta
+    for i in range(0, len(thetaRange)):
+        for j in range(0, len(phiRange)):
+            
+            ## calculate psi (using belo function)
+            a = phiRange[j]      
+            beta = 0
+            
+            cosa = np.cos(a)
+             
+            if cosa>0:
+                                       
+                # computing trig functions once for each patch
+                costheta = np.cos(thetaRange[i])
+                sintheta = np.sin(thetaRange[i])
+                sinphi = np.sin(phiRange[j])
+                cosphi = np.cos(phiRange[j])      
+                
+                ### r_hat vector components in x, y, z coordinates (eq 15 - pavlov2000)
+                r_hat = np.array([sinphi*costheta, sinphi*sintheta, cosphi])
+
+                ## get magnetic field at all points (eq 13 - pavlov2000) 
+                B_vec = (B_p/2)*((2+f)*np.dot(r_hat, m_hat)*r_hat - f*m_hat)  # in the basis vectors of (x,y,z)
+       
+                ## find phi' using By' and Bx'.  
+                xi = np.arctan2(B_vec[1], B_vec[0])               # phi' is the angle between the x' axis and the projection of B_vec_prime onto the x'y' plane
+                
+                ## angle between B_vec and normal to the surface
+                B = np.linalg.norm(B_vec)
+                
+                ## angle between magnetic field and the unit wave vector at the surface
+                Theta_prime = np.arccos(B_vec[2]/B)  
+                              
+                # degree of linear polarization
+                p_L = linear_polarization(Theta_prime, B, E)
+                
+                ## compute flux in Q stokes parameter from this area element
+                FQ.append(g_R*dA*cosa*(I_o-I_e)*p_L*np.cos(2*(xi)))
+                FU.append(g_R*dA*cosa*(I_o-I_e)*p_L*np.sin(2*(xi)))
+                F.append(g_R*dA*cosa*(I_o+I_e))
+
+
+    return np.nansum(FQ), np.nansum(FU), np.nansum(F)
 
 
 ###############################################################
@@ -271,9 +372,9 @@ class observation:
     def __init__(self, star, phaseRange, E, I_map=[]):
         
         if I_map == []:
+            
             stokesParamComputed = [stokes(m, star, E, res='low') for m in phaseRange]
             stokesOutput = np.transpose(stokesParamComputed)
-            totalI = [F_I(m, star, E, res='low') for m in phaseRange]
         else:
             stokesParamComputed = [stokes_PolarCap(m, star, E, I_map, res='low') for m in phaseRange]
             stokesOutput = np.transpose(stokesParamComputed)     
@@ -284,5 +385,29 @@ class observation:
         self.FU = stokesOutput[1]
         
         # Total Intensity
-        self.I = totalI
+        self.I = stokesOutput[2]
+        
+
+# create a class for observations (which ignore lensing effects)
+'''
+Am observation class represents a single observation of a given neutron star, 
+for a specific phase range (in radians), at a given energy.
+'''
+class observation_nolensing:
+    def __init__(self, star, phaseRange, E, I_map=[]):
+        
+        if I_map == []:
+            
+            stokesParamComputed = [stokes_nolensing(m, star, E, res='low') for m in phaseRange]
+            stokesOutput = np.transpose(stokesParamComputed)
+        else:
+            stokesParamComputed = [stokes_PolarCap(m, star, E, I_map, res='low') for m in phaseRange]
+            stokesOutput = np.transpose(stokesParamComputed)     
+       
+        # Computed Stokes Parameters
+        self.FQ = stokesOutput[0]
+        self.FU = stokesOutput[1]
+        
+        # Total Intensity
+        self.I = stokesOutput[2]
         
--- modules/starcatalog.py
+++ modules/starcatalog.py
@@ -8,43 +8,47 @@ eta (angle between magnetic axis and spin axis), i (angle between LOS and spin a
 B_p (magnetic field strength at the poles in 10^12 Gauss)
 '''
 class star:
-    def __init__(self, R, M, eta, i, B_p):
+    def __init__(self, R, M, nu, zeta, B_p):
         self.radius = R  # in terms of schwarzschild radius
         self.mass = M    # in terms of solar masses
-        self.eta = np.deg2rad(eta)   # angle between magnetic axis and spin axis
-        self.i = np.deg2rad(i)       # angle between LOS and spin axis
+        self.eta = np.deg2rad(nu)   # angle between magnetic axis and spin axis
+        self.i = np.deg2rad(zeta)       # angle between LOS and spin axis
         self.B_p = B_p   # magnetic field strength at the poles (in 10^12 Gauss)
 
     
 # Changing Angle between Magnetic Axis and Spin Axis
-NS1 = star(3, 1.6, 0, 0, 1.4)
-NS2 = star(3, 1.6, 45, 0, 1.4)
-NS3 = star(3, 1.6, 90, 0, 1.4)
+NS1 = star(2.5, 1.6, 0, 0, 1.4)
+NS2 = star(2.5, 1.6, 45, 0, 1.4)
+NS3 = star(2.5, 1.6, 90, 0, 1.4)
 
-NS4 = star(3, 1.6, 0, 30, 1.4)
-NS5 = star(3, 1.6, 45, 30, 1.4)
-NS6 = star(3, 1.6, 90, 30, 1.4)
+NS4 = star(2.5, 1.6, 0, 30, 1.4)
+NS5 = star(2.5, 1.6, 45, 30, 1.4)
+NS6 = star(2.5, 1.6, 90, 30, 1.4)
 
-NS7 = star(3, 1.6, 0, 60, 1.4)
-NS8 = star(3, 1.6, 45, 60, 1.4)
-NS9 = star(3, 1.6, 90, 60, 1.4)
+NS7 = star(2.5, 1.6, 0, 60, 1.4)
+NS8 = star(2.5, 1.6, 45, 60, 1.4)
+NS9 = star(2.5, 1.6, 90, 60, 1.4)
 
-NS10 = star(3, 1.6, 0, 90, 1.4)
-NS11 = star(3, 1.6, 45, 90, 1.4)
-NS12 = star(3, 1.6, 90, 90, 1.4)
+NS10 = star(2.5, 1.6, 0, 90, 1.4)
+NS11 = star(2.5, 1.6, 45, 90, 1.4)
+NS12 = star(2.5, 1.6, 90, 90, 1.4)
 
 # Changing Observation Spectra
-NS13 = star(3, 1.6, 40, 50, 1)
+NS13 = star(3, 1.6, 45, 60, 1)
 
 # Changing Magnetic Field Strength
-NS14 = star(3, 1.6, 45, 60, 0.1)
-NS15 = star(3, 1.6, 45, 60, 1)
-NS16 = star(3, 1.6, 45, 60, 10)
+NS24 = star(2.5, 1.6, 45, 60, 0.01)
+NS14 = star(2.5, 1.6, 45, 60, 0.1)
+NS15 = star(2.5, 1.6, 45, 60, 1)
+NS16 = star(2.5, 1.6, 45, 60, 10)
+NS23 = star(2.5, 1.6, 45, 60, 100)
 
 # Changing compactness
 NS17 = star(2.2, 1.6, 45, 60, 0.1)
 NS18 = star(2.4, 1.6, 45, 60, 0.1)
 NS19 = star(2.6, 1.6, 45, 60, 0.1)
 NS20 = star(2.8, 1.6, 45, 60, 0.1)
+NS21 = star(3.0, 1.6, 45, 60, 0.1)
+NS22 = star(5, 1.6, 45, 60, 0.1)
 
 # and so on
